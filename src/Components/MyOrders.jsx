739ca5b3b2ff3be3d47616d573364f4b1a867468
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-08-30 16:44:03 +0600

    resolve

--- src/Components/MyOrders.jsx
+++ src/Components/MyOrders.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../firebase/FirebaseAuthProvider";
+import axios from "axios";
 
 const MyOrders = () => {
+  const { user } = useContext(AuthContext);
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    axios(`http://localhost:3000/my-orders/${user?.email}`)
+      .then((res) => {
+        setOrders(res?.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [user?.email]);
   return (
-    <div>
-      <h2>My Orders</h2>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-7">
+      {orders.map((order) => (
+        <div
+          key={order._id}
+          className="bg-white shadow-md rounded-2xl p-5 border hover:shadow-lg transition"
+        >
+          <h3 className="text-lg font-semibold text-gray-800 mb-2">
+            Order ID: <span className="text-sm text-gray-600">{order._id}</span>
+          </h3>
+
+          <p className="text-gray-700 mb-4">
+            <span className="font-medium">Customer Email:</span>{" "}
+            {order.customarEmail}
+          </p>
+
+          <button className="w-full bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-red-600 transition">
+            Cancel Order
+          </button>
+        </div>
+      ))}
     </div>
   );
 };
